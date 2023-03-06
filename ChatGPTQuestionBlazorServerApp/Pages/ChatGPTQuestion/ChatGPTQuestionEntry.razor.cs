4e21621d45eade4effb44e8413bb6b21f61eda99
John Tomlinson <jtomlinson@brightree.com>
2023-03-06 18:26:11 +0000

    Improved UI

--- ChatGPTQuestionBlazorServerApp/Pages/ChatGPTQuestion/ChatGPTQuestionEntry.razor.cs
+++ ChatGPTQuestionBlazorServerApp/Pages/ChatGPTQuestion/ChatGPTQuestionEntry.razor.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
 namespace ChatGPTQuestionBlazorServerApp.Pages.ChatGPTQuestion
 {
     public partial class ChatGPTQuestionEntry
     {
-        private string questionForChatGPT = string.Empty;
+        private string QuestionForChatGPT { get; set; } = string.Empty;
+
+        private string ErrorMessage { get; set; } = string.Empty;
 
         private string answerFromChatGPT = string.Empty;
 
@@ -10,12 +14,45 @@ namespace ChatGPTQuestionBlazorServerApp.Pages.ChatGPTQuestion
 
         private bool answerReceived = false;
 
-        public async Task AskChatGPTAQuestionAsync()
+        private bool isValidQuestion = false;
+
+        private async Task AskChatGPTAQuestionAsync()
         {
+            ErrorMessage = string.Empty;
             buttonClicked = true;
             answerReceived = false;
-            answerFromChatGPT = await ChatGPTCompletionService.PerformTextCompletion(questionForChatGPT);
-            answerReceived = true;
+
+            if (string.IsNullOrWhiteSpace(QuestionForChatGPT))
+            {
+                ErrorMessage = "Please enter a valid question for Chat GPT";
+
+                isValidQuestion = false;
+
+                return;
+            }
+
+            isValidQuestion = true;
+
+            try
+            {
+                answerFromChatGPT = await ChatGPTCompletionService.PerformTextCompletion(QuestionForChatGPT);
+                answerReceived = true;
+            }
+            catch(Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                isValidQuestion = false;
+            }
+        }
+
+        private void ClearQuestion()
+        {
+            QuestionForChatGPT = string.Empty;
+            answerFromChatGPT = string.Empty;
+            ErrorMessage = string.Empty;
+            ErrorMessage = string.Empty;
+            buttonClicked = false;
+            answerReceived = false;
         }
     }
 }
